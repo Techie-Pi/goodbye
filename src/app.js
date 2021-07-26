80f224e2dab5074bd51872f171d72e935821e322
TechiePi <techiepi@pm.me>
2021-07-26 22:44:56 +0200

    feat: note access
    /:noteId/:password

--- src/app.js
+++ src/app.js
@@ -1,21 +1,39 @@
+const Path = require("path");
 const { promisify } = require("util");
 
 const express = require("express");
+const handlebars = require("express-handlebars");
 const app = express();
 
-const Path = require("path");
-
 const api = require("./routes/api");
+const { canAccessNote, removeNote } = require("./notes/noteManagement");
 
-const render = promisify(app.render);
+app.set("view engine", "handlebars");
+
+app.engine("handlebars", handlebars({
+    layoutsDir: Path.resolve("views/layouts")
+}));
 
 app.use("/api/v1", api);
 
 app.get("/", (req, res) => {
-    res.sendFile(Path.resolve("pages/index.html"))
+    res.render("main", { layout: "index" })
 })
 
+app.get("/:noteId/:password", async (req, res) => {
+    try {
+        const { result: canAccess, _note: note } = await canAccessNote({ id: req.params.noteId, password: req.params.password });
 
+        if(!canAccess) { res.status(403).end() }
+
+        await removeNote({ id: req.params.noteId });
+
+        res.render("note", { layout: "index", note })
+    }
+    catch(e) {
+        res.status(500).end();
+    }
+})
 
 app.listen(process.env.PORT || 8090, () => {
     console.log(`Running on port ${process.env.PORT || 8090}`);
