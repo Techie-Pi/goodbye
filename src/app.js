7708a5080a223ae002571bf47283ccb5838752f7
TechiePi <techiepi@pm.me>
2021-07-26 22:25:18 +0200

    fix: invalid file path index

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const { promisify } = require("util");
 const express = require("express");
 const app = express();
 
+const Path = require("path");
+
 const api = require("./routes/api");
 
 const render = promisify(app.render);
@@ -10,8 +12,10 @@ const render = promisify(app.render);
 app.use("/api/v1", api);
 
 app.get("/", (req, res) => {
-    res.sendFile("pages/index.html")
+    res.sendFile(Path.resolve("pages/index.html"))
 })
+
+
 
 app.listen(process.env.PORT || 8090, () => {
     console.log(`Running on port ${process.env.PORT || 8090}`);
